ce4a4c4f1e7cf3acd4dd9d5e21bf3e2a8c2f0f72
JamesKeatings <jk@SURFACELAPTOP2.localdomain>
2020-10-26 14:46:35 +0000

    Included Doppler.py

---
+++ Year1/Doppler.py
@@ -0,0 +1,36 @@
+#/usr/bin/env python
+#
+#	Doppler.py
+#       Python script for UWS first year lab
+#       Doppler Shift experiment
+#       James Keatings
+#       James.Keatings@uws.ac.uk
+#
+#
+
+
+import time
+import matplotlib.pyplot as plt
+
+#Start experiment
+print('Welcome to the Doppler Shift first year lab experiment. In this lab the affect of motion of frequency will be demonstrated.')
+raw_input('When you are ready to begin please press Enter to continue.')
+
+#open file to write results to in case of error
+f = open("results_doppler.txt","w")
+
+#Explaination of the experiment
+print('A sound emitter producing a constant noise is passing a stationary microphone. When the emitter is moving towards the microphone the wavelength of the sound is compressed, and when the emitter is moving away from the microphone the wavelength is stretched. The compression/stretching of the wavelength increases/decreases the frequency of the sound that is heard.')
+
+#Move the emitter
+print('The sound emitter will now pass by the microphone at *insert speed here"')
+
+start_time = time.time()
+#START EMITTER
+#RECORD FREQUENCY FROM MICROPHONE
+move_time = round(time.time() - start_time,3)
+#PLOT GRAPH OF FREQUENCY
+
+
+#End experiment
+print('You have now taken all measurements required for this experiment.\nPlease refer to the labscript for the next steps.\n')
