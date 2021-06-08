7299bdc478e82fdc579dbba3ccafac8557ee28df
JamesKeatings <james.m.keatings@gmail.com>
2021-06-08 12:59:09 +0100

    Updated transistor support, experiment now works

--- Year1/Gravity.py
+++ Year1/Gravity.py
@@ -19,8 +19,8 @@ import sys
 #Set up button
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #BUTTON
+GPIO.setup(16, GPIO.OUT)                            #TRANSISTOR FOR MAGNET
 
 #open file to write results to in case of error
 f = open("results_gravity.txt","w")
@@ -30,29 +30,30 @@ f = open("results_gravity.txt","w")
 height = 1
 
 #BEGINING OF THE EXPERIMENT
-print('Welcome to the Gravitional Constant lab experiment.\nIn this lab you will measure the acceleration due to gravity of multiple objects of varying masses, and observe how the acceleration differs.\n')
+print('Welcome to the Gravitional Constant lab experiment.\nIn this lab you will measure the acceleration due to gravity of multiple objects of varying masses, and observe how the acceleration differs.')
 input("When you are ready to begin please press Enter to continue.")
 
 
 #Explanation for stduent
-print('There are 3 objects of different masses suspended using an electromagnet. When the current is cut to the magnet the object will fall, after an amount of time it will fall onto a switch. Using the time difference between the begining and end of the fall you will calculate the acceleration due to gravity experienced by the 3 objects.\n')
-input("When you are ready to drop the first object, please press Enter")
+print('\nThere are 3 objects of different masses suspended using an electromagnet. When the current is cut to the magnet the object will fall, after an amount of time it will fall onto a switch. Using the time difference between the begining and end of the fall you will calculate the acceleration due to gravity experienced by the 3 objects.')
+input("When you are ready to attach the first object to the magnet, please press Enter")
 
 
-#Place the ball?
-#GPIO.input(x)=GPIO.HIGH
-
+#Place the ball
+print('\nThe magnet is now actived. Please place the ball onto the magnet.')
+GPIO.output(16, 1)
+input('When the metal ball is secured and you are ready to drop, please press enter.')
 
 #Drop and time first object
-print('The object will drop in....\n3...')
+print('\nThe object will drop in....\n3......')
 time.sleep(1)
-print('2...')
+print('2....')
 time.sleep(1)
-print('1...')
+print('1..')
 time.sleep(1)
 print('0!')
 #turn off magnet
-#GPIO.input(x) = GPIO.HIGH
+GPIO.output(16, 0)
 start_time = time.time()
 
 #wait for switch
@@ -62,12 +63,12 @@ while True:
 		fall_time = round(time.time() - start_time,3)
 		break
 
-print('The first object took ' + repr(fall_time) + ' seconds to fall')
+print('\nThe first object took ' + repr(fall_time) + ' seconds to fall')
 acceleration = round(2 * height / fall_time**2,3)
 print('For a ' + repr(height) + ' m fall, this gives acceleration equal to ' + repr(acceleration) + ' m/s^2')
 f.write("Object 1 = " + repr(fall_time) + " s\n")
 
 
 #END OF THE EXPERIMENT
-print('You have now taken all measurements required for this experiment.\nPlease refer to the labscript for the next steps.\n')
+print('You have now taken all measurements required for this experiment.\nPlease refer to the labscript for the next steps.')
 sys.exit()
