d51e3f99d7c9882b71191474113b699401899459
JamesKeatings <james.m.keatings@gmail.com>
2021-06-22 16:26:15 +0100

    set up pins and included basic framework

--- Year1/Capacitance.py
+++ Year1/Capacitance.py
@@ -5,6 +5,7 @@
 #       Measuring capacitance experiment
 #       James Keatings
 #       James.Keatings@uws.ac.uk
+#       Including scripts based on voltage.py from www.kookye.com
 #
 #
 
@@ -15,10 +16,19 @@ import sys
 
 
 #Set up GPIO
-trigger_pin = 16
+SPICKL = 11         #ADC pin 1
+SPIMISO = 9         #ADC pin 2
+SPIMOSI = 10        #ADC pin 3
+SPICS = 8            #ADC pin 4
+moswitch_pin = 16    #MOSWITCH pin
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(trigger_pin, GPIO.OUT)                            #TRANSISTOR FOR MAGNET
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(SPICLK, GPIO.OUT)
+GPIO.setup(SPIMISO, GPIO.IN)
+GPIO.setup(SPIMOSI, GPIO.OUT)
+GPIO.setup(SPICS, GPIO.OUT)
+GPIO.setup(moswitch_pin, GPIO.OUT)
 
 #open file to write results to in case of error
 f = open("results_capacitance.txt","w")
@@ -43,4 +53,6 @@ input("\nPlease change the resistor and not down the value. When you are done pl
 
 #END OF THE EXPERIMENT
 print('You have now taken all measurements required for this experiment.\nPlease refer to the labscript for the next steps.')
+
+GPIO.cleanup()
 sys.exit()
